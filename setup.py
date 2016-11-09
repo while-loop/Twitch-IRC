b9b9ce46ece5817ceed1af13d299cb4f3903340c
Anthony Alves <aalves@fit.edu>
2016-11-09 15:32:40 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='twitchirc',
+    version='0.0.1',
+    description='Python library to interface with Twitch IRC (https://github.com/justintv/Twitch-API/blob/master/IRC.md)',
+    author='Anthony Alves',
+    author_email='cvballa3g0@gmail.com',
+    url='https://github.com/while-loop/Twitch-IRC',
+    packages=['twitchirc'],
+    scripts=[],
+    install_requires=["mock==2.0.0"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
