469fb2a854628113ba34988f9db7e253499861b8
Anthony Alves <cvballa3g0@gmail.com>
2016-12-14 20:04:14 -0500

    Updating docs and adding example usage

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Twitch-IRC
 ==========
 
-# LIBRARY IS IN DEVELOPMENT
+# !!!LIBRARY IS IN DEVELOPMENT!!!
 
 [![GitHub version](https://badge.fury.io/gh/while-loop%2FTwitch-IRC.svg)](https://badge.fury.io/gh/while-loop%2FTwitch-IRC) [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://opensource.org/licenses/mit-license.php)   
 
@@ -15,7 +15,9 @@ Features
 - Verbose error messages
 - Complies with Twitch IRC Command & Message [rate limits](https://help.twitch.tv/customer/portal/articles/1302780-twitch-irc)
     - 50 JOINs per 15 seconds
-    - 20 commands/messages per 30 seconds 
+    - 20 commands/messages per 30 seconds
+    - Optional Moderator only bot
+        - Mod only bots get an increase rate of 100 commands per 30 secs
 
 Installation
 ------------
@@ -29,16 +31,16 @@ Or
 
 Basic Usage
 -----------
+Example of simple chat bot [here](docs/echo.py).
 
+Example of complex chat bot [here](docs/example2.py).
 
 Future Updates / TODO
 ---------------------
 - Transparent Server reconnection (Needs testing)
 - Well [tested](tests/) and [documented](docs/)
-- Optional Moderator only bot
-    - Mod only bots get an increase rate of 100 commands per 30 secs
-    - Need to enforce that the bot is a mod before sending message
-- Custom Command & Message rate limits
+- SSL
+- Receive and parse Tags from twitch channel
 
 Changelog
 ---------
--- docs/echo.py
+++ docs/echo.py
@@ -1,3 +1,59 @@
 """
     This example demonstrates the use of chat callbacks
 """
+import os
+import random
+import sys
+import time
+
+from twitchirc.exception import IRCException, AuthenticationError
+from twitchirc.irc import IRC
+
+
+class MyIRC(IRC):
+    def onMessage(self, channel, viewer, message):
+        print "[onMessage] Received message from {} in #{}: {}".format(viewer, channel, message)
+
+        if random.random() > .99:
+            response = "@{}.. Cool story, bro.".format(viewer)
+            print "Sending response:\n\t{}".format(response)
+            self.sendMessage(channel, response)
+
+    def onCommand(self, channel, viewer, command, value):
+        print "[onCommand] Received command from {} in #{}: {}:{}".format(viewer, channel, command, value)
+
+        if command == "exit":
+            response = "Goodbye, world!"
+            print "Sending response:\n\t{}".format(response)
+            self.sendMessage(channel, response)
+            time.sleep(1)
+            self.shutdown()
+
+    def onJoinPart(self, channel, viewer, state):
+        print "[onJoinPart] {} {}ED #{}".format(viewer, state, channel)
+
+    def onMode(self, channel, viewer, state):
+        print "[onMode] {}OP'd {} in #{}".format(state, viewer, channel)
+
+    def onClearChat(self, channel, viewer):
+        if viewer:
+            print "[onClearChat] Chat cleared for {} in #{}".format(viewer, channel)
+        else:
+            print "[onClearChat] Chat cleared for all in #{}".format(channel)
+
+
+if __name__ == '__main__':
+    # construct IRC with oauth and twitch username
+    irc = MyIRC("oauth:abcdefghijklmnopqrstuvwxyz", "username")
+
+    try:
+        irc.connect()  # connect to the irc server and login
+    except (AuthenticationError, IRCException) as e:
+        print >> sys.stderr, e.message
+        sys.exit(255)
+
+    irc.joinChannels(["loltyler1", "trick2g"])
+
+    irc.serverForever()  # keep the main program from exiting
+    print "done"
+    os.kill(os.getpid(), 9)
