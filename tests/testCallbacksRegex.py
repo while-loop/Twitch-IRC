27cd25bf6a0e27ff5edd92b93a0675d5ff3ea1e6
Anthony Alves <aalves@fit.edu>
2017-02-06 17:30:28 -0500

    Formatting test files for new callbacks

--- tests/testCallbacksRegex.py
+++ tests/testCallbacksRegex.py
@@ -1,19 +1,9 @@
 import inspect
-import socket
 import time
 import unittest
 from types import FunctionType
 
-from mock import MagicMock
-
 from twitchirc.irc import IRC
-
-
-class mIRC(IRC):
-    sock = MagicMock(name='socket', spec=socket.socket)
-
-    def createSocket(self):
-        return mIRC.sock
 
 
 class TestCallbacks(unittest.TestCase):
@@ -33,17 +23,6 @@ class TestCallbacks(unittest.TestCase):
 
     def tearDown(self):
         time.sleep(.005)  # allow the callbacks to be executed
-        try:
-            self.chat.shutdown()
-        except AttributeError:
-            pass
-
-    def setAndConnect(self, msg, shebang='!', clz=mIRC):
-        recvs = [self.login, '', '']
-        recvs.extend(list(msg))
-        mIRC.sock.recv.side_effect = recvs
-        self.chat = clz("nooauth", TestCallbacks.USER, cmdShebang=shebang)
-        self.chat.connect()
 
     def test_on_message(self):
         MESSAGE = "testmessage"
@@ -56,14 +35,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(message, MESSAGE)
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onMessage(self, channel, viewer, message):
                 callback(self, channel, viewer, message)
 
-        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PRIVMSG #{channel} :{message}\r\n".format(
+        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PRIVMSG #{channel} :{message}".format(
             viewer=TestCallbacks.VIEWER, channel=TestCallbacks.CHANNEL, message=MESSAGE)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_command(self):
         SHEBANG = "!"
@@ -79,14 +59,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(value, VALUE)
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onCommand(self, channel, viewer, command, value):
                 callback(self, channel, viewer, command, value)
 
-        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PRIVMSG #{channel} :{shebang}{command} {value}\r\n".format(
+        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PRIVMSG #{channel} :{shebang}{command} {value}".format(
             viewer=TestCallbacks.VIEWER, channel=TestCallbacks.CHANNEL, shebang=SHEBANG, command=COMMAND, value=VALUE)
 
-        self.setAndConnect(msg, SHEBANG, TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER, cmdShebang=SHEBANG)
+        chat.onResponse(msg)
 
     def test_on_command_with_shebang(self):
         SHEBANG = '#'
@@ -102,14 +83,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(value, VALUE)
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onCommand(self, channel, viewer, command, value):
                 callback(self, channel, viewer, command, value)
 
-        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PRIVMSG #{channel} :{shebang}{command} {value}\r\n".format(
+        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PRIVMSG #{channel} :{shebang}{command} {value}".format(
             viewer=TestCallbacks.VIEWER, channel=TestCallbacks.CHANNEL, shebang=SHEBANG, command=COMMAND, value=VALUE)
 
-        self.setAndConnect(msg, SHEBANG, TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER, cmdShebang=SHEBANG)
+        chat.onResponse(msg)
 
     def test_on_join(self):
         func = inspect.stack()[0][3]
@@ -121,14 +103,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(state, IRC.JOIN)
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onJoinPart(self, channel, viewer, state):
                 callback(self, channel, viewer, state)
 
-        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv JOIN #{channel}\r\n".format(
+        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv JOIN #{channel}".format(
             viewer=TestCallbacks.VIEWER, channel=TestCallbacks.CHANNEL)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_part(self):
         func = inspect.stack()[0][3]
@@ -140,14 +123,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(state, IRC.PART)
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onJoinPart(self, channel, viewer, state):
                 callback(self, channel, viewer, state)
 
-        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PART #{channel}\r\n".format(
+        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PART #{channel}".format(
             viewer=TestCallbacks.VIEWER, channel=TestCallbacks.CHANNEL)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_mode_op(self):
         OPCODE = IRC.OP
@@ -160,14 +144,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(opcode, OPCODE)
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onMode(self, channel, viewer, opcode):
                 callback(self, channel, viewer, opcode)
 
-        msg = ":jtv MODE #{channel} {op}o {viewer}\r\n".format(
+        msg = ":jtv MODE #{channel} {op}o {viewer}".format(
             viewer=TestCallbacks.VIEWER, channel=TestCallbacks.CHANNEL, op=OPCODE)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_mode_deop(self):
         OPCODE = IRC.DEOP
@@ -180,14 +165,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(opcode, OPCODE)
             TestCallbacks.tests[func] = True
 
-        msg = ":jtv MODE #{channel} {op}o {viewer}\r\n".format(
+        msg = ":jtv MODE #{channel} {op}o {viewer}".format(
             viewer=TestCallbacks.VIEWER, channel=TestCallbacks.CHANNEL, op=OPCODE)
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onMode(self, channel, viewer, opcode):
                 callback(self, channel, viewer, opcode)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_notice_already_r9k_on(self):
         ID = IRC.ID_ALREADY_R9K_ON
@@ -201,14 +187,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(message, MSG)
             TestCallbacks.tests[func] = True
 
-        msg = "@msg-id={id} :tmi.twitch.tv NOTICE #{channel} :{message}\r\n".format(
+        msg = "@msg-id={id} :tmi.twitch.tv NOTICE #{channel} :{message}".format(
             id=ID, channel=TestCallbacks.CHANNEL, message=MSG)
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onNotice(self, channel, msgid, message):
                 callback(self, channel, msgid, message)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_notice_emote_only_on(self):
         ID = IRC.ID_EMOTE_ONLY_ON
@@ -222,14 +209,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(message, MSG)
             TestCallbacks.tests[func] = True
 
-        msg = "@msg-id={id} :tmi.twitch.tv NOTICE #{channel} :{message}\r\n".format(
+        msg = "@msg-id={id} :tmi.twitch.tv NOTICE #{channel} :{message}".format(
             id=ID, channel=TestCallbacks.CHANNEL, message=MSG)
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onNotice(self, channel, msgid, message):
                 callback(self, channel, msgid, message)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_host_target(self):
         HOSTING = "hostingchannel"
@@ -244,14 +232,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(amount, AMOUNT)
             TestCallbacks.tests[func] = True
 
-        msg = ":tmi.twitch.tv HOSTTARGET #{hosting} :{target} {amount}\r\n".format(
+        msg = ":tmi.twitch.tv HOSTTARGET #{hosting} :{target} {amount}".format(
             hosting=HOSTING, target=TARGET, amount=AMOUNT)
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onHostTarget(self, hosting, target, amount):
                 callback(self, hosting, target, amount)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_stop_host_target(self):
         HOSTING = "hostingchannel"
@@ -266,14 +255,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(amount, AMOUNT)
             TestCallbacks.tests[func] = True
 
-        msg = ":tmi.twitch.tv HOSTTARGET #{hosting} :- {amount}\r\n".format(
+        msg = ":tmi.twitch.tv HOSTTARGET #{hosting} :- {amount}".format(
             hosting=HOSTING, amount=AMOUNT)
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onHostTarget(self, hosting, target, amount):
                 callback(self, hosting, target, amount)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_clear_chat_viewer(self):
         func = inspect.stack()[0][3]
@@ -284,14 +274,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(viewer, self.VIEWER)
             TestCallbacks.tests[func] = True
 
-        msg = ":tmi.twitch.tv CLEARCHAT #{channel} :{viewer}\r\n".format(
+        msg = ":tmi.twitch.tv CLEARCHAT #{channel} :{viewer}".format(
             channel=self.CHANNEL, viewer=self.VIEWER)
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onClearChat(self, channel, viewer):
                 callback(self, channel, viewer)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_clear_chat_channel(self):
         func = inspect.stack()[0][3]
@@ -302,14 +293,15 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(viewer, None)
             TestCallbacks.tests[func] = True
 
-        msg = ":tmi.twitch.tv CLEARCHAT #{channel}\r\n".format(
+        msg = ":tmi.twitch.tv CLEARCHAT #{channel}".format(
             channel=self.CHANNEL)
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onClearChat(self, channel, viewer):
                 callback(self, channel, viewer)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_usernotice(self):
         MSG = "Yo, NiCe StReAm BrUh!"
@@ -321,62 +313,65 @@ class TestCallbacks(unittest.TestCase):
             self.assertEqual(message, MSG)
             TestCallbacks.tests[func] = True
 
-        msg = ":tmi.twitch.tv USERNOTICE #{channel} :{message}\r\n".format(
+        msg = ":tmi.twitch.tv USERNOTICE #{channel} :{message}".format(
             channel=self.CHANNEL, message=MSG)
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onUserNotice(self, channel, message):
                 callback(self, channel, message)
 
-        self.setAndConnect(msg, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_response_override(self):
         func = inspect.stack()[0][3]
-        LINE = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PRIVMSG #{channel} :{message}\r\n" \
+        msg = ":{viewer}!{viewer}@{viewer}.tmi.twitch.tv PRIVMSG #{channel} :{message}" \
             .format(viewer=self.VIEWER, channel=self.CHANNEL, message="This is a message")
 
         def callback(irc, line):
             self.assertIsNotNone(irc)
-            self.assertEqual(line, LINE.replace("\r\n", ""))
+            self.assertEqual(line, msg.replace("\r\n", ""))
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onResponse(self, line):
                 callback(self, line)
 
-        self.setAndConnect(LINE, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onResponse(msg)
 
     def test_on_ping_override(self):
         func = inspect.stack()[0][3]
-        LINE = "PING :tmi.twitch.tv\r\n"
 
         def callback(irc):
             self.assertIsNotNone(irc)
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onPing(self):
                 callback(self)
 
-        self.setAndConnect(LINE, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onPing()
 
     def test_on_reconnect_override(self):
         func = inspect.stack()[0][3]
-        LINE = "RECONNECT :tmi.twitch.tv\r\n"
 
         def callback(irc):
             self.assertIsNotNone(irc)
             TestCallbacks.tests[func] = True
 
-        class TESTIRC(mIRC):
+        class TESTIRC(IRC):
             def onReconnect(self):
                 callback(self)
 
-        self.setAndConnect(LINE, clz=TESTIRC)
+        chat = TESTIRC("noauth", TestCallbacks.USER)
+        chat.onReconnect()
 
     def test_zzzzz_tests_passed(self):
         for func, passed in TestCallbacks.tests.iteritems():
             self.assertTrue(passed, "Failed Function: {}".format(func))
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/testIRCConn.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestIRC(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_login_passes_with_correct_password(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
